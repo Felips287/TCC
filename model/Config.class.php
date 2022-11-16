89661d304d08ddc82ab11703740dc2a69bc35fe4
Felips287 <109115463+Felips287@users.noreply.github.com>
2022-11-16 13:37:02 -0300

    Add files via upload

---
+++ model/Config.class.php
@@ -0,0 +1,32 @@
+<?php
+
+Class Config{
+
+
+const SITE_URL = "http://localhost";
+const SITE_PASTA = "TCC";
+const SITE_NOME = "Loja Fashion Moda";
+const SITE_EMAIL_ADM = "lojaTCCfashionmoda@gmail.com";
+
+
+const BD_HOST = "localhost";
+const BD_USER = "root";
+const BD_SENHA = "";
+const BD_BANCO = "lojatcc";
+const BD_PREFIX = "";
+
+
+//INFORMAÇÕES PHP MAILLER
+
+const EMAIL_HOST = "smtp.gmail.com";
+const EMAIL_USER = "lojaTCCfashionmoda@gmail.com";
+const EMAIL_NOME = "Contato Loja Fashion Moda";
+const EMAIL_SENHA = "fashionmoda_@123";
+const EMAIL_PORTA = 587;
+const EMAIL_SMTPAUTH = true;
+const EMAIL_SMPTSECURE = "tls";
+const EMAIL_COPIA = "lojaTCCfashionmoda@gmail.com";
+
+}
+
+?>
